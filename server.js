d7ef0347b411a19ac58b40e531af62aab222d6d4
Minh Huynh <mhuynh09@gmail.com>
2018-02-21 15:48:17 +1100

    Fixed RegExp

--- server.js
+++ server.js
@@ -5,22 +5,20 @@ const lily = require('./engine');
 const PORT = 1000;
 
 // url: /solve?numbers=25,100,75,50,6,4&target=856
-const validRequest = /^\/solve\?numbers=(,?\d\d?,?){6}&target=\d{3}/; 
+const validRequest = /^\/solve\?numbers=(,?\d{1,3},?){6}&target=\d{3}/;
 
 const server = http.createServer((request, response) => {
     if (validRequest.test(request.url)) {
         let urlInfo = url.parse(request.url, true);
         let numbers = JSON.parse(`[${urlInfo.query.numbers}]`);
         let target = Number.parseInt(urlInfo.query.target);
-        if (Array.isArray(numbers)) {
-            let solution = lily(numbers, target);
-            response.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : '*'});
-            response.write(JSON.stringify(solution));
-        } else {
-            response.statusCode = 400;    
-        }
+        console.log(`numbers: [${numbers}]\ntarget: ${target}`);
+        response.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : '*'});
+        let solution = lily(numbers, target);
+        console.log('done');
+        response.write(JSON.stringify(solution));
     } else {
-        response.statusCode = 404;
+        response.writeHead(400, {'Access-Control-Allow-Origin' : '*'});
     }
     response.end();
 }).listen(PORT);
